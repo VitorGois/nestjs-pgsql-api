3098d850f7953a901bfdfac9d80aeb2458b609fe
vitor.gois <vitorcgois777@gmail.com>
2022-01-05 22:34:51 -0300

    add class validator in AuthLoginDto

--- src/auth/auth.dto/auth.login.dto.ts
+++ src/auth/auth.dto/auth.login.dto.ts
@@ -1,4 +1,12 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
+
 export class AuthLoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(200)
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(5)
   password: string;
 }
