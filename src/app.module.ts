a821699e299e317e361411a34fedeb123463a525
vitor.gois <vitorcgois777@gmail.com>
2022-01-07 15:48:50 -0300

    configure nest-winston log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig, winstonConfig } from './configs';
+import { WinstonModule } from 'nest-winston';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    UserModule, 
+    AuthModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    }
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@
 require('dotenv').config();
 
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
 import { AppModule } from './app.module';
+import { winstonConfig } from './configs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = WinstonModule.createLogger(winstonConfig);
+  const app = await NestFactory.create(AppModule, { logger });
   const port = process.env.PORT;
   await app.listen(port);
 }
